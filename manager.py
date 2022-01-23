4cd3d1a78b5fe229701c08a3bc6a5d115751bf61
vicente <vicente@espressolabs.com.br>
2022-01-23 16:14:20 -0300

    reimplemented workspace manager with use of classes

--- manager.py
+++ manager.py
@@ -1,30 +1,26 @@
 
 import os, sys
 import argparse as agp
-import generator
-import libsmanager
-import projmanager
-
+import workspace as wp
 
 
 if __name__=="__main__":
 
-	
+	current_workspace = wp.Workspace(os.environ.get("WORKSPACE_FOLDER"))
+
 	main_parser = agp.ArgumentParser(sys.argv[0],
 	description= "Window C/C++ project manager")
 	######## main arguments
-	main_parser.add_argument('--version',action='version',version="win-cman 0.1")
+	main_parser.add_argument('--version',action='version',version=f"win-cman 0.1")
 
+	#separating parsers
 	subparser = main_parser.add_subparsers(help="sub-commands help",dest="subcommand")
-
 	library_parser = subparser.add_parser("libs",help="manage libraries and dependencies")
 	project_parser = subparser.add_parser("proj",help="manage current project, it's libraries and dependencies")
 	generator_parser = subparser.add_parser("gen",help="Project generator")
 
 	##project generation START
-	
 	generator_parser.add_argument("name", type=str, default="project")
-
 	#git options
 	git_parser = generator_parser.add_argument_group("git")
 	git_parser.add_argument('--git', 
@@ -36,25 +32,17 @@ if __name__=="__main__":
 	git_parser.add_argument("-git-clone", 
 		help="change the behavior of repository initialization for cloning",
 		action="store_true",default=False)	
-
-	#library option
-
 	#make option
 	make_parser = generator_parser.add_argument_group("make")
 	make_parser.add_argument("--make",
 		help="skips asking for makefile generation",
 		action =agp.BooleanOptionalAction)
-
 	make_parser.add_argument("--make-sub",
 		help="skips asking for makefile generation",
 		action="store_true",default=False)
-
-	make_parser.add_argument("--list","-ls",help="List all libraries installed",action="store_true",default=False)
-	
 	##project generation END
 
 	##libraries management START
-	
 	library_parser.add_argument("--list","-ls",help="List all libraries installed",action="store_true",default=False)
 
 	# parser.add_argument('--use-lib',"--ul",
@@ -64,18 +52,11 @@ if __name__=="__main__":
 
 	##libraries management END
 
+	##projects manager START
+	project_parser.add_argument("--list","-ls",help="List all projects you currently working on",action="store_true",default=False)
+	##projects manager END
 
-
+	##run actions
 	args = main_parser.parse_args(sys.argv[1:])
-	
-	if args.subcommand == "gen" :
-		#generator implementation
-		generator.generate( args.name, args)
-	elif args.subcommand == "libs" :
-		#library manager implementation
-		libsmanager.run(args)
-	elif args.subcommand == "proj" :
-		#project manager implemntation
-		pass
-	else:
-		print("No arguments provided")
+	current_workspace.printInfo()
+	current_workspace.parseAction(args)
--- workspace.py
+++ workspace.py
@@ -1,33 +1,15 @@
 from dataclasses import dataclass, field
+from distutils import command
 from enum import Enum, auto, unique
 import json
+from msilib.schema import Property
+from xml.etree.ElementInclude import default_loader
 import colorama as cl
 import os
 
+from numpy import lib
+
 cl.init()
-
-@unique
-class PType(Enum):
-	UNDEFINED = auto()
-	C = auto()
-	CPP = auto()
-	NODE = auto()
-	STYLED = auto()
-	_count = auto()
-
-
-@dataclass
-class Project:
-	name: str
-	path: str
-	type: PType
-
-	@staticmethod
-	def generate_project(name, path, type=PType.UNDEFINED, showStats = False):
-		if type is PType.C:
-			print("generating a new C project")
-		elif type is PType.CPP:
-			print("generation a new C++ project")
 
 @dataclass
 class Library:
@@ -43,10 +25,29 @@ class Library:
 	links:   list[str] 	= field(default_factory=list)
 
 	def validate(self):
-		pass
+		return True
 
-	@staticmethod
-	def loaddef(spath, recursion = -1, showStats = False):
+	def load_libdef_json(self, showStats=False):
+		def_file = os.path.join(self.path, "libdef.json")
+		has_def_file = os.path.exists(def_file)
+		if has_def_file:
+			try:
+				data = {}
+				with open(def_file, "r") as _catalog:
+					data = json.load(_catalog)
+				return ( data , def_file ) 
+			except json.JSONDecodeError as error:
+				print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR{cl.Style.RESET_ALL}: invalid file {def_file}!")
+				print(error)
+		return None
+
+	def load_libdef_data(self,showStats=False):
+		config = self.load_libdef_json(showStats=showStats)
+		if config is not None:
+			(_data, _file) = config
+			#update current variables
+			if "name" in _data: self.name = _data["name"];
+			return _data
 		return None
 
 	@staticmethod
@@ -56,25 +57,96 @@ class Library:
 			return None
 		if showStats:
 			print(f"\t{cl.Fore.CYAN}lib{cl.Style.RESET_ALL}: {name} : {lib_path}")
-
-		return True	
+		return True
 
 	@staticmethod
-	def check_libdef_json(spath, showStats = False):
-		dir_files = [f for f in  os.listdir(spath) if os.path.isfile( os.path.join(spath, f) )]
-		if "libdef.json" in dir_files:
-			lib = Library.loaddef(spath,showStats = showStats)
-			return lib
+	def find(start_path, skip=[], recurion = -1, showStats = False):
+		#walk through all folders and set them as projects
+		for dir in [d for d in os.listdir(start_path) if (os.path.isdir(os.path.join(start_path,d)) and not d in skip) ]:
+			library = Library(name=dir, path=os.path.join(start_path, dir))
+			if library.load_libdef_data() is not None:	
+				yield library
+
+		# for dir in [os.path.join(start_path, _d) for _d in  os.listdir(start_path)]:
+		# 	_isdir = os.path.isdir(dir)
+		# 	if _isdir and not dir in skip:
+		# 		print(f"found file at:{dir}")
+
+
+
+@unique
+class PType(Enum):
+	UNDEFINED = auto()
+	C = auto()
+	CPP = auto()
+	NODE = auto()
+	STYLED = auto()
+	_count = auto()
+
+@dataclass
+class Project:
+	name: str
+	path: str
+	type: PType = field(default=PType.UNDEFINED)
+	dependencies: list = field(default_factory=list)
+
+	@staticmethod
+	def generate_project(name, path, type=PType.UNDEFINED, showStats = False):
+		if type is PType.C:
+			print("generating a new C project")
+		elif type is PType.CPP:
+			print("generation a new C++ project")
+
+	def load_cman_proj_json(self, showStats=False):
+		config_file = os.path.join(self.path, "cman-proj.json")
+		has_project_config = os.path.exists(config_file)
+		if has_project_config:
+			try:
+				data = {}
+				with open(config_file, "r") as _catalog:
+					data = json.load(_catalog)
+				return ( data , config_file ) 
+			except json.JSONDecodeError as error:
+				print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR{cl.Style.RESET_ALL}: invalid file {config_file}!")
+				print(error)
 		return None
-		
-			
+
+	def load_project_data(self,showStats=False):
+		config = self.load_cman_proj_json(showStats=showStats)
+		if config is not None:
+			(_data, _file) = config
+			#update current variables
+			if "name" in _data: self.name = _data["name"];
+			if "type" in _data:
+				if _data["type"] in PType.__members__:
+					self.type = PType[ _data["type"] ]
+			if "dependencies" in _data: self.dependencies = _data["dependencies"]
+			return _data
+		return None
 
 	@staticmethod
-	def find(spath, skip=[], recurion = -1, showStats = False):
-		for dir in [os.path.join(spath, _d) for _d in  os.listdir(spath)]:
-			_isdir = os.path.isdir(dir)
-			if _isdir and not dir in skip:
-				print(f"found file at:{dir}")
+	def find(start_path, skip=[], recurion = -1, showStats = False):
+		#walk through all folders and set them as projects
+		for dir in [d for d in os.listdir(start_path) if (os.path.isdir(os.path.join(start_path,d)) and not d in skip) ]:
+			project = Project(name=dir, path=os.path.join(start_path, dir))
+			if project.load_project_data() is not None:
+				print(project)
+				yield project
+
+	# @staticmethod
+	# def load(name,start_path, data, recursion = -1, showStats = False):
+	# 	project = Project(name=name, path=start_path)
+	# 	project.check_valid()
+
+
+	# 	lib_path = os.path.join(start_path, name) if not "path" in data else os.path.join(start_path, data["path"])
+	# 	if not os.path.exists(lib_path):
+	# 		return None
+	# 	if showStats:
+	# 		print(f"\t{cl.Fore.CYAN}lib{cl.Style.RESET_ALL}: {name} : {lib_path}")
+
+	# 	return True	
+
 
 
 @dataclass
@@ -91,8 +163,7 @@ class Workspace:
 			exit(1)
 		pass
 	
-	
-	def load_library_catalog_data(self, path:str):
+	def load_catalog_data(self, path:str, required=True):
 		catalog_file = os.path.join(path, "catalog.json");
 		has_catalog = os.path.exists(catalog_file)
 		if has_catalog:
@@ -102,12 +173,54 @@ class Workspace:
 					data = json.load(_catalog)
 				return ( data , catalog_file ) 
 			except json.JSONDecodeError as error:
-				print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR: {cl.Style.RESET_ALL} invalid catalog.json!")
+				print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR{cl.Style.RESET_ALL}: invalid catalog.json!")
 				print(error)
-				exit(1)
+				if required:
+					exit(1)
 		else:
-			print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR: {cl.Style.RESET_ALL} {catalog_file} not found!")
-			exit(1)
+			if required:
+				print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR{cl.Style.RESET_ALL}: {catalog_file} not found!")
+				exit(1)
+			else:
+				print(f"{cl.Back.YELLOW}{cl.Fore.BLACK}WARN{cl.Style.RESET_ALL}: {catalog_file} not found!")
+		return None
+
+	##libraries START#############################################
+	# TODO: add libraries
+	# def add_library_to_catalog_json(self, path:str, library: Library):
+		
+	# 	#a bit of safety check
+	# 	if not Library.validate():
+	# 		print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR{cl.Style.RESET_ALL}: trying to save an invalid library: {library.name} ")
+	# 		exit(1)
+	# 		return None
+
+	# 	catalog_file = os.path.join(path, "catalog.json");
+	# 	has_catalog = os.path.exists(catalog_file)
+	# 	if has_catalog:
+	# 		try:
+	# 			data = {}
+	# 			with open(catalog_file, "rw") as _catalog:
+	# 				data = json.load(_catalog)
+	# 				data.update()
+
+	# 			return ( data , catalog_file ) 
+	# 		except json.JSONDecodeError as error:
+	# 			print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR: {cl.Style.RESET_ALL} invalid catalog.json!")
+	# 			print(error)
+	# 			exit(1)
+	# 	else:
+	# 		print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR: {cl.Style.RESET_ALL} {catalog_file} not found!")
+	# 		exit(1)
+	def findLibraries(self,path=None,showStats=False):
+		find_path = path if path is not None else os.path.join(self.path,"libs")
+		found_libs:list[Library] = [l for l in Library.find(find_path,skip=[],showStats=showStats)]
+
+		if len( found_libs ) < 1:
+			print(f"{cl.Back.YELLOW}{cl.Back.BLACK}NO LIBRARIES FOUND{cl.Style.RESET_ALL}: Any file was found at {find_path}")
+		else:
+			for lib in found_libs:
+				print(f"{lib}")
 
 	def check_libraries(self, file_path, data, recursion = -1,showStats = False):
 		total_valid_libraries = 0;
@@ -136,10 +249,50 @@ class Workspace:
 			return None
 
 		print(f"{cl.Style.DIM}{cl.Back.CYAN}{cl.Fore.BLACK}libraries in {libs_dir} {cl.Style.RESET_ALL}")
-		( data, catalog_path ) = self.load_library_catalog_data(libs_dir)
+		( data, catalog_path ) = self.load_catalog_data(libs_dir)
 		self.check_libraries(catalog_path, data, showStats=True)
 		
 		return True
+
+	##libraries END #############################################
+
+	##projects START #############################################
+
+	def parse_projects(self,data, showStats=False):
+		pass
+
+	def list_projects(self):
+		proj_dir = os.path.join(self.path, "projects")
+		if not os.path.exists(proj_dir):
+			return None
+
+		#check for project catalog
+		#catalog = self.load_catalog_data(proj_dir,required=False)
+
+		project_list:list[Project] = []
+		skip_folders = []
+		
+
+		# #load projects from catalog if not None
+		# if catalog is not None: 
+		# 	(projects, path) = catalog;
+		# 	for project in projects:
+		# 		print(project)
+			
+		folder_projects = [ p for p in Project.find(proj_dir,skip=skip_folders)]
+		if len(project_list) < 1:
+			print(f"{cl.Style.DIM}{cl.Back.CYAN}{cl.Fore.BLACK}No projects found in {proj_dir}{cl.Style.RESET_ALL}")
+		else:
+			print(f"{cl.Style.DIM}{cl.Back.CYAN}{cl.Fore.BLACK}Projects in {proj_dir} {cl.Style.RESET_ALL}")
+	
+		
+		# print(f"{cl.Style.DIM}{cl.Back.CYAN}{cl.Fore.BLACK}Projects in {proj_dir} {cl.Style.RESET_ALL}")
+		# Project.
+
+		# Project.find(proj_dir)
+		return True
+
+	##projects END   #############################################
 
 	def settupManual(self):
 		print(f"{cl.Back.RED}{cl.Fore.BLACK}ERROR:{cl.Style.RESET_ALL}\tinvalid workspace settup\n")
@@ -164,11 +317,20 @@ class Workspace:
 		pass
 
 	def printInfo(self):
-		print(f"current workspace folder: {self.path}\n\n")
-		if self.list_libraries() is None:
-			print(f"{cl.Back.YELLOW}{cl.Fore.BLACK}WARN{cl.Style.RESET_ALL}: libs folder not found!")
+		print("Workspace:")
+		print(f"current workspace folder: {self.path}")
+	
+	def parseAction(self,args):
+		if args.subcommand == "libs":
+			if args.list: self.list_libraries();
+				
+		elif args.subcommand == "proj":
+			if args.list: self.list_projects();
 
-
+		elif args.subcommand == "gen":
+			print("command gen")
+		else:
+			print("No arguments provided!")
 
 if __name__=="__main__":
 	current_workspace = Workspace(os.environ.get("WORKSPACE_FOLDER"))
